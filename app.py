fb119d81d54c15408ce55fa197cc5847f0bff3a5
stefbez <stefbez@hotmail.com>
2020-12-05 20:39:34 +0000

    Add sign up functionalitty including validation for names, emails and passwords

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,29 @@ mongo = PyMongo(app)
 def library():
     books = mongo.db.books.find()
     return render_template("library.html", books=books)
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            flash("Looks like you've already signed up!")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "first_name": request.form.get("first_name").lower(),
+            "last_name": request.form.get("last_name").lower(),
+            "email": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        session["user"] = request.form.get("email").lower()
+        flash("Welcome to The Book Club, {}!".format(request.form.get("first_name")))
+    return render_template("sign_up.html")
 
 
 if __name__ == "__main__":
