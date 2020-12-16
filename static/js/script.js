f7a7c54a1f9de94d54822ac43bc7cba6a44f48c3
stefbez <stefbez@hotmail.com>
2020-12-16 11:33:06 +0000

    Add formatting and comments to all files

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@
     $('select').formSelect();
     $('.scrollspy').scrollSpy();
 
+    // Dropdown fix for materialize - from the code institute task manager project https://github.com/Code-Institute-Solutions/TaskManagerAuth
     validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
